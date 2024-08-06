0e0385d114abd53cdb5337a4daabb515b395a042
Jackie <actions@github.com>
2024-08-05 21:28:26 -0400

    CTA Widget

--- src/Components/CTAWidget/CTAWidget.jsx
+++ src/Components/CTAWidget/CTAWidget.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './CTAWidget.css';
 
 const CTAWidget = () => {
   return (
-    <div>CTAWidget</div>
-  )
+    <div className="cta-widget">
+      <h3>Start your day with a clear mind</h3>
+      <p>Your word, your world</p>
+      <button className='white-button'>
+        <Link to='./journal'>Start writing</Link>
+      </button>
+    </div>
+  );
 }
 
-export default CTAWidget
+export default CTAWidget;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Scrollbar } from 'react-scrollbars-custom';
 import ClockWidget from '../../Components/ClockWidget/ClockWidget';
 import PresetWidget from '../../Components/PresetWidget/PresetWidget';
 import PromptWidget from '../../Components/PromptWidget/PromptWidget';
@@ -8,13 +9,15 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className="widget-container">
-      <div className="widget widget1 glass"><ClockWidget /></div>
-      <div className="widget widget2 glass"><CTAWidget /></div>
-      <div className="widget widget3 glass"><AchievementsWidget /></div>
-      <div className="widget widget4 glass"><PresetWidget /></div>
-      <div className="widget widget5 glass"><PromptWidget /></div>
+    <Scrollbar>
+      <div className="widget-container">
+        <div className="widget widget1 glass"><ClockWidget /></div>
+        <div className="widget widget2 glass"><CTAWidget /></div>
+        <div className="widget widget3 glass"><AchievementsWidget /></div>
+        <div className="widget widget4 glass"><PresetWidget /></div>
+        <div className="widget widget5 glass"><PromptWidget /></div>
     </div>
+    </Scrollbar>
   );
 }
 
